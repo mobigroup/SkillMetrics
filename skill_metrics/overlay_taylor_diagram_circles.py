a0544f5ed47d846036866916eaf9f818201a3e99
Alexey Pechnikov <pechnikov@mobigroup.ru>
2017-11-08 20:12:15 +0700

    Fix for 'ValueError: shape mismatch: value array of shape (2,) could not be broadcast to indexing result of shape (1,)'

--- skill_metrics/overlay_taylor_diagram_circles.py
+++ skill_metrics/overlay_taylor_diagram_circles.py
@@ -49,8 +49,8 @@ def overlay_taylor_diagram_circles(axes,cax,option):
 
     # now really force points on x/y axes to lie on them exactly
     inds = range(0,len(th),(len(th)-1) // 4)
-    xunit[inds[1:3:2]] = np.zeros(2)
-    yunit[inds[0:4:2]] = np.zeros(3)
+    xunit[inds[1:3:2]] = np.zeros(1)
+    yunit[inds[0:4:2]] = np.zeros(2)
     
     # DRAW RMS CIRCLES:
     # ANGLE OF THE TICK LABELS
